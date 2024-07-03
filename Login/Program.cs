6fd9345b455ad09b8ce8b1952f52812c606ae1d4
OB <oliverbadger@aol.com>
2024-07-03 09:45:03 +0100

    Add project files.

---
+++ Login/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+
+// Variables
+string username = "user";
+string localUsername = "user";
+string password = "pass";
+string localPassword = "pass";
+
+// Ternary operator to authenticate password
+string display = (username == localUsername && password == localPassword) ? "Correct!" : "Incorrect Details!";
+
+// Display outcome of authentication
+Console.WriteLine(display);
